9ed7c027b237dba6a98ad8b33b19d123ce4ccfa8
sasha <kremlsa@yandex.ru>
2021-05-24 15:51:26 +0700

    Initial commit

---
+++ src/main/java/org/example/springJpa/dao/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package org.example.springJpa.dao;
+
+
+import org.example.springJpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    public List<Employee> findAllByName(String name);
+
+}
